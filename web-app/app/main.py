c941626f9f3d87dd9fe954d69684be1ebb1035ef
trungnq0202 <trungnqcs2000@gmail.com>
2021-12-08 19:06:10 +0700

    add cors for backend

--- web-app/app/main.py
+++ web-app/app/main.py
@@ -1,10 +1,21 @@
+from time import daylight
 from fastapi import Depends, FastAPI
 from starlette.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware 
 
 from routers import enviromentMonitor, humanEntryAndExit, qrCode
 
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(enviromentMonitor.router)
 app.include_router(humanEntryAndExit.router)
