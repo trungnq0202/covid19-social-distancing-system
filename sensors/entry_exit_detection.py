fb3ee75bf6f6b538e9079a6d682daaaf4d72ec6c
tminhquang00 <tminhquang00@gmail.com>
2021-12-13 20:09:55 +0700

    change people entry and exit detection

--- sensors/entry_exit_detection.py
+++ sensors/entry_exit_detection.py
@@ -6,8 +6,11 @@ from lcd16x2 import display_message
 from apiHelper import RequestsApi
 
 from grove.grove_ultrasonic_ranger import GroveUltrasonicRanger
+from second_sensor import get_dist
 
 server = RequestsApi()
+u_sensor = GroveUltrasonicRanger(16)
+
 
 def update_num_people(num_people):
     response = server.put("humanEntryAndExit/update/people/{}".format(num_people))
@@ -34,7 +37,6 @@ def calculate_motion(distances):
 def detect():
 
     # Ultrasonic sensor connected to port D16
-    u_sensor = GroveUltrasonicRanger(16)
     num_people = 0
     update_num_people(num_people)
     distances = []
@@ -76,11 +78,54 @@ def detect():
         else:
             dist = u_sensor.get_distance()
             if dist <= 100:
-                distances.append()
+                distances.append(dist)
 
         print("Number of people:" + str(num_people))
         time.sleep(1)
       
 
+
+def detect_entry():
+
+    sensor_in_distances = []
+    sensor_out_distances = []
+    num_people = 0 
+    
+    while True:
+        sensor_in = False
+        sensor_out = False
+        # exit case
+        if len(sensor_in_distances) == 3 and abs(calculate_motion(sensor_in_distances)) > 10:
+            sensor_in = True
+            print("Somebody exits")
+            notify_person_action("exit")
+            display_message("Good Bye")
+            update_num_people(num_people)
+            sensor_in_distances.clear()
+            sensor_out_distances.clear()
+        # Entry case
+        if len(sensor_in_distances) == 3 and abs(calculate_motion(sensor_out_distances)) > 10:
+            sensor_out = True
+            if num_people < 5:
+                print("Somebody entry")
+                display_message("Welcome")
+                num_people += 1
+                notify_person_action("enter")
+                update_num_people(num_people)
+                sensor_in_distances.clear()
+                sensor_out_distances.clear()
+            else: 
+                display_message("Too many people")
+                print("Too many people")
+                sensor_in_distances.clear()
+                sensor_out_distances.clear()
+
+        sensor_in_distances.append(u_sensor.get_distance())
+        sensor_out_distances.append(get_dist())
+        print("Number of people:" + str(num_people))
+        time.sleep(1)
+
+
+
 if __name__ == "__main__":
-    detect()
+    detect_entry()
