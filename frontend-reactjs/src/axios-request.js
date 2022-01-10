c8c23abe0847111b55cf64b82a5ff3dd886ff29f
tminhquang00 <tminhquang00@gmail.com>
2022-01-11 04:44:44 +0700

    add fetch data function to the UI

--- frontend-reactjs/src/axios-request.js
+++ frontend-reactjs/src/axios-request.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'http://localhost:8000/'
-    baseURL: 'http://172.20.10.3:8000/'
+    baseURL: 'http://localhost:8000/'
+    //baseURL: 'http://172.20.10.3:8000/'
 });
 
 export default instance;
