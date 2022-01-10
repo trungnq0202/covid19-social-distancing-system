ce4dd28fe69dda9929c8b3bcdbdcdff688dfeab3
QuangTran2706 <48042533+QuangTran2706@users.noreply.github.com>
2022-01-10 23:08:31 +0700

    change entry and exit detection

--- sensors/_entry_exit_detection.py
+++ sensors/_entry_exit_detection.py
@@ -1,15 +1,10 @@
 import time
-import statistics
+ 
 from cv2 import fastAtan2
-import requests
-import asyncio
-
-from requests.models import Response
-# from threading import Timer
 from starlette.datastructures import URL
 
 from qrCode import turn_on_qr_reader, keep_alive
-from lcd16x2 import display_message
+from lcd16x2 import display
 from apiHelper import RequestsApi
 from grove.grove_mini_pir_motion_sensor import GroveMiniPIRMotionSensor
 
@@ -22,28 +17,26 @@ m_sensor_entry = GroveMiniPIRMotionSensor(6)
 m_sensor_exit = GroveMiniPIRMotionSensor(2)
 
 
-# m_sensor_entry_flag = False
+m_sensor_entry_flag = False
 m_sensor_exit_flag = False
 num_people = 0
+total_enter = 0
+total_exit = 0
 
-def update_num_people(num_people):
-    response = server.put("humanEntryAndExit/update/people/{}".format(num_people))
+def update_num_people():
+    global num_people
+    global total_enter
+    global total_exit
+    response = server.put("humanEntryAndExit/update/people/{}/{}/{}".format(num_people, total_enter, total_exit))
     if response.ok is False:
             print(response.raise_for_status())
 
-def notify_person_action(action):
-    response = None
-    if action == "enter":
-        response = server.post("humanEntryAndExit/PersonEnter")
-    else:
-        response = server.post("humanEntryAndExit/PersonExit")
-    if response.ok is False:
-            print(response.raise_for_status())
 
 def set_update_people_server_flag(flag):
     response = server.post("roomMonitor/setFlag/{}".format(flag))
     if response.ok is False:
             print(response.raise_for_status())
+
 
 def check_valid_qr():
     #Request the server to allow streaming the qr code scaning process
@@ -72,54 +65,68 @@ def check_valid_qr():
 
 def handle_person_entry():
     global num_people
+    global total_enter
+
     if num_people == 5:
-        display_message("Too many people")
-    elif num_people > 0:
-        set_update_people_server_flag(True)
-        if not check_valid_qr():
-            return
-        display_message("Welcome")
+        display("Too many people")
+    elif num_people >= 0:
+        # if not check_valid_qr():
+        #     return
         num_people += 1
-        notify_person_action("enter")
-        update_num_people(num_people)
+        total_enter += 1
+        display("Welcome")
+        update_num_people()
         set_update_people_server_flag(False)
+
 
 def handle_person_exit():
     global num_people
+    global total_exit
+
     num_people -= 1
-    notify_person_action("exit")
-    display_message("Good Bye")
-    update_num_people(num_people)
+    total_exit += 1
+    display("Good Bye")
+    update_num_people()
+
 
 def m_sensor_entry_callback():
-    # global m_sensor_entry_flag
-    global m_sensor_exit_flag
-    global num_people
-    #Entry case
-    if m_sensor_exit_flag == False:
-        print("Somebody enters")
-        handle_person_entry()
+    global m_sensor_entry_flag
+    m_sensor_entry_flag = True
 
-    #Exit case
-    else:
-        print("Somebody exits")
-        handle_person_exit()
-        m_sensor_exit_flag = False
 
 def m_sensor_exit_callback():
     global m_sensor_exit_flag
     m_sensor_exit_flag = True
 
-def detect_entry():
-    num_people = 0
- 
-    m_sensor_entry.on_detect = m_sensor_entry_callback
-    m_sensor_exit.on_detect = m_sensor_exit_callback
+
+def detect():
+    
+    global m_sensor_entry_flag
+    global m_sensor_exit_flag
+    global num_people
 
     while True:
+        m_sensor_entry.on_detect = m_sensor_entry_callback
+        m_sensor_exit.on_detect = m_sensor_exit_callback
+
+        #Entry case
+        if m_sensor_entry_flag == True:
+           for _ in range(3):
+               m_sensor_exit.on_detect = handle_person_entry
+               time.sleep(0.5)
+
+
+        #Exit case
+        if m_sensor_exit_flag == True:
+            for _ in range(3):
+               m_sensor_entry.on_detect = handle_person_exit
+               time.sleep(0.5)
+
+        m_sensor_exit_flag = False
+        m_sensor_entry_flag = False
         time.sleep(0.2)
     
 
 
 if __name__ == '__main__':
-    detect_entry()
+    detect()
--- sensors/lcd16x2.py
+++ sensors/lcd16x2.py
@@ -1,4 +1,5 @@
 import time
+from multiprocessing import Process
 
 from grove.display import JHD1802
 # LCD 16x2 Characters
@@ -10,3 +11,14 @@ def display_message(message):
     dobj.write(message)
     time.sleep(10)
     dobj.clear()
+
+
+def display(message):
+    action_process = Process(target=display_message, args=(message, ))
+
+    # We start the process and we block for 5 seconds.
+    action_process.start()
+    action_process.join(timeout=5)
+
+    # We terminate the process.
+    action_process.terminate()
